9d9008c966eb6df8eb1b521b62f75040a72585fb
RUMINATOR <88246935+vishalmaurya11@users.noreply.github.com>
2022-09-15 19:25:10 +0530

    Updates

---
+++ precache-manifest.fe59177ef0c9ad555c941026102cfa0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "639a655fc2c9377d4ef5",
+    "url": "/my-app/static/css/main.5c47bcac.chunk.css"
+  },
+  {
+    "revision": "639a655fc2c9377d4ef5",
+    "url": "/my-app/static/js/main.639a655f.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "84f276c702a51aecf73e3fcf3e6ffb0d",
+    "url": "/my-app/index.html"
+  }
+];
